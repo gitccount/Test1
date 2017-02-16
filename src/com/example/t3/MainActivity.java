c50a4af3ddd5263cfc3d8ece829dde3332988a3b
gitccount <dennti@163.com>
2017-02-16 23:00:22 +0800

    加了一丢丢代码

--- src/com/example/t3/MainActivity.java
+++ src/com/example/t3/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//我就加了一点点代码
 		setContentView(R.layout.activity_main);
 	}
 

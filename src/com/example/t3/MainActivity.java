d7babbe3a297fff40acee574b342fd68226f96f9
gitccount <dennti@163.com>
2017-02-17 10:50:34 +0800

    更新了分支

--- src/com/example/t3/MainActivity.java
+++ src/com/example/t3/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//分支更新了
 		//10:38的天路
 		//这是一条神奇的天路
 		//我就加了一点点代码
